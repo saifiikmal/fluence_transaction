77bf36ab294c04c1e307db2565b7c9153efee655
saifiikmal <saifi.ikmal@gmail.com>
2023-07-07 18:42:22 +0800

    Changed timestamp to version for send_transaction method

--- services/node/src/transactions_impl.rs
+++ services/node/src/transactions_impl.rs
@@ -25,7 +25,7 @@ impl Storage {
                 method TEXT NOT NULL,
                 nonce INTEGER NOT NULL,
                 token_id TEXT,
-                node_timestamp INTEGER NOT NULL
+                version INTEGER NOT NULL
             );",
             TRANSACTIONS_TABLE_NAME
         );
@@ -39,7 +39,7 @@ impl Storage {
 
     pub fn write_transaction(&self, transaction: Transaction) -> Result<String, ServiceError> {
         let s = format!(
-            "insert into {} (hash, token_key, token_id, from_peer_id, host_id, status, data_key, data, public_key, alias, timestamp,meta_contract_id, method, error_text, nonce, node_timestamp) values ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');",
+            "insert into {} (hash, token_key, token_id, from_peer_id, host_id, status, data_key, data, public_key, alias, timestamp, meta_contract_id, method, error_text, nonce, version) values ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');",
             TRANSACTIONS_TABLE_NAME,
             transaction.hash,
             transaction.token_key,
@@ -56,7 +56,7 @@ impl Storage {
             transaction.method,
             transaction.error_text,
             transaction.nonce,
-            transaction.node_timestamp,
+            transaction.version,
         );
 
         let result = self.connection.execute(s);
@@ -196,12 +196,12 @@ pub fn read(statement: &Statement) -> Result<Transaction, ServiceError> {
         data: statement.read::<String>(6)?,
         public_key: statement.read::<String>(7)?,
         alias: statement.read::<String>(8)?,
-        timestamp: statement.read::<i64>(9)?,
+        timestamp: statement.read::<i64>(9)? as u64,
         error_text: statement.read::<String>(10)?,
         meta_contract_id: statement.read::<String>(11)?,
         method: statement.read::<String>(12)?,
         nonce: statement.read::<i64>(13)?,
         token_id: statement.read::<String>(14)?,
-        node_timestamp: statement.read::<i64>(15)? as u64,
+        version: statement.read::<i64>(15)?,
     })
 }
